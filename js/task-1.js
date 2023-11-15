17f4565ae1e93eeae38955210c24c99aaf6d75b2
Satanw8you <trusnikolaj41@gmail.com>
2023-11-15 20:25:31 +0200

    finish

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,12 @@
-function slugify(title) {
-  return title.toLowerCase().replaceAll(' ', '-');
+function isEnoughCapacity(products, containerSize) {
+  let total = 0;
+  for (const product in products) {
+    total += products[product];
+  }
+  return total < containerSize;
 }
-console.log(slugify('Arrays for begginers'));
-console.log(slugify('English for developer'));
-console.log(slugify('Ten secrets of JavaScript'));
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));
